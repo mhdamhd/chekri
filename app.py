a88b712d521aba6a6a4b5d2c2f9304d879b0bf7a
mhd <mohammad.aeoub@tatweer.sy>
2024-10-16 14:58:33 +0300

    changed app and Procfile

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from dash import Dash, html
 
+# Define the app
 app = Dash(__name__)
 
+# App layout
 app.layout = html.Div("Hello, Dash!")
+# Expose the underlying WSGI server instance
+server = app.server
 
+# Ensure this block is included for local development
 if __name__ == "__main__":
     app.run_server(debug=True)
